6e1a673a5f0cae037431bb2695d05e23734bc4e7
nikcapcha@mail.ru <nikolai20114.ru@mail.ru>
2025-05-10 15:20:29 +0300

    Initial commit

---
+++ Source/gameC/gameC.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "gameC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gameC, "gameC" );
---
+++ Source/gameC/gameCGameModeBase.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gameCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEC_API AgameCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	public:
+		AgameCGameModeBase();
+};
