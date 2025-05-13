d91cc8105bdc17d86b418e486298046949a00a01
nikcapcha@mail.ru <nikolai20114.ru@mail.ru>
2025-05-13 06:36:30 +0300

    Делегаты, UFUNCTION

--- Source/gameC/Public/TestingNewActor.h
+++ Source/gameC/Public/TestingNewActor.h
@@ -7,6 +7,13 @@
 #include "Components/StaticMeshComponent.h"
 #include "TestingNewActor.generated.h"
 
+//объявление делегата
+//Все делегаты назавния объявляются с большой буквы F
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnColorChangedTestingNewActor, const FLinearColor&, Color, const FString&, Name);
+
+//так как этот делегат только в с++, мы можем не указывать имя актора в блюпринтах
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnTimerFinishedTestingNewActor, AActor*);
+
 //Класс ENUM отвечающий за тип движения
 //UENUM это макрос который расширяет возможности enum и позволяет выводить его в едитор
 //Все enum должны начинать с заглавной буквы E , EMovementType
@@ -21,25 +28,27 @@ enum class EMovementTypeTestingActor : uint8
 //структура тот же класс, но все поля public
 //структура при наследовании имеет все поля как public
 //можно объединять переменные для их удобной группировки
+//
+//BlueprintReadWrite, позволяет получать доступ к переменные на blueprint graph
 USTRUCT(BlueprintType)
 struct FGeometrydataTestingActor
 {
     GENERATED_USTRUCT_BODY()
 
     //Две переменные, отвечающие за амплитуду и частоту колебаний для логики движения объектов
-    UPROPERTY(EditAnywhere, Category = "Movement")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,Category = "Movement")
     float Amplitude = 50.0f;
 
-    UPROPERTY(EditAnywhere, Category = "Movement")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
     float Frequency = 2.0f;
 
     //Создание переменной MoveType типа enum MovementType
     //который содержит переходные значия STATIC, SIN
-    UPROPERTY(EditAnywhere, Category = "Movement")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
     EMovementTypeTestingActor MoveType = EMovementTypeTestingActor::STATIC;
 
     //Цвет меша
-    UPROPERTY(EditAnywhere, Category = "Design")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Design")
     FLinearColor Color = FLinearColor::Black;
 
     //Таймер 
@@ -63,6 +72,14 @@ public:
 
     //Функция для изменения GeometryData 
     void SetGeometryData(const FGeometrydataTestingActor& Data) { GeometryData = Data; }
+
+    //позволяет вызвать функцию из блюпринт графа 
+    UFUNCTION(BlueprintCallable)
+    FGeometrydataTestingActor GetGeometryData() const {return GeometryData;}
+
+    //создание делегатов 
+    FOnColorChangedTestingNewActor OnColorChanged;
+    FOnTimerFinishedTestingNewActor OnTimerFinished;
 
 protected:
     // Called when the game starts or when spawned
@@ -107,7 +124,7 @@ protected:
 
     //переменная хранящая в себе структуру
     //В ней хранятся для вывода другие переменные
-    UPROPERTY(EditAnywhere, Category = "GeometryData")
+    UPROPERTY(EditAnywhere,BlueprintReadWrite,  Category = "GeometryData")
     FGeometrydataTestingActor GeometryData;
 
 public:
