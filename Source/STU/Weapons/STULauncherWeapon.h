fb210e6d9b684f2fe4e420a1d87bb89d054bc042
nikcapcha@mail.ru <nikolai20114.ru@mail.ru>
2025-05-18 21:55:18 +0300

    projectile, spawn

--- Source/STU/Weapons/STULauncherWeapon.h
+++ Source/STU/Weapons/STULauncherWeapon.h
@@ -6,12 +6,19 @@
 #include "STUBaseWeapon.h"
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class STU_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    virtual void StartFire() override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+    TSubclassOf<ASTUProjectile> ProjectileClass;
+
+    virtual void MakeShot() override;
 };
---
+++ Source/STU/Weapons/STUProjectile.h
@@ -0,0 +1,24 @@
+// Shoot Them Up Game, All Right Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUProjectile.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class STU_API ASTUProjectile : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ASTUProjectile();
+
+protected:
+    UPROPERTY(VisibleDefaultsOnly, Category = "Weapon")
+    USphereComponent* CollisionComponent;
+
+    virtual void BeginPlay() override;
+};
